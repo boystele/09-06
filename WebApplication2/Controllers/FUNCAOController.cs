b45af2ae050ed6c2a93d9de547bfe6b4b2033abe
robson rodrigues <robsonrodrigues_12@hotmail.com>
2019-06-09 04:15:46 -0300

    Add project files.

---
+++ WebApplication2/Controllers/FUNCAOController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class FUNCAOController : Controller
+    {
+        EMPRESAEntities ctx = new EMPRESAEntities();
+
+        public ActionResult Index()
+        {
+
+            return View(ctx.Funcao.ToList());
+        }
+
+        public ActionResult Details(int id)
+        {
+            var detalhes = ctx.Funcao.Find(id);
+            ctx.SaveChanges();
+            return View(detalhes);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Funcao create)
+        {
+            if (ModelState.IsValid)
+            {
+                ctx.Funcao.Add(create);
+                ctx.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+
+        public ActionResult Delete(int id)
+        {
+            EMPRESAEntities ctx = new EMPRESAEntities();
+            var objetoDeletar = ctx.Funcao.Find(id);
+            ctx.Funcao.Remove(objetoDeletar);
+            ctx.SaveChanges();
+            return View(objetoDeletar);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Funcao cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                EMPRESAEntities ctx = new EMPRESAEntities();
+                ctx.Entry(cliente).State = System.Data.Entity.EntityState.Deleted;
+                return RedirectToAction("Index");
+            }
+
+            return View(cliente);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var editar = ctx.Funcao.Find(id);
+            ctx.SaveChanges();
+            return View(editar);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Funcao FUNCAO)
+        {
+            if (ModelState.IsValid)
+            {
+                ctx.Entry(FUNCAO).State = System.Data.Entity.EntityState.Modified;
+                ctx.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/MetadataType/Funcao_MD.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    [MetadataType(typeof(Funcao_MD))]
+    public partial class Funcao { };
+    public class Funcao_MD
+    {
+        [DisplayName("Funcão Id")]
+        public int FuncaoId { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigadorio")]
+        [DisplayName("Descricao Funcão")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigadorio")]
+        [StringLength(5, ErrorMessage = "No Maximo 5 Caracteres")]
+        public string Siglas { get; set; }
+    }
+}
